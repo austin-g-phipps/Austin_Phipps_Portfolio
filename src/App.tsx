c4b57e3336f93e007ec80e05b0d4d98663e969d3
unknown <austin.g.phipps@gmail.com>
2026-01-17 17:02:27 -0800

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home'
 import { NotFound } from './pages/NotFound'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route index element = {<Home />}/>
         <Route path="*" element = {<NotFound />}/>
       </Routes>
-    </BrowserRouter>
+    </Router>
     </>
   )
 }
